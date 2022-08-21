d79ee2a509259bb789946d7b800f7264b4c1b204
bárbara-valdez-zaidan <babivaldez@gmail.com>
2022-08-21 16:29:23 -0300

    criei um programa para retornar valor positivo, negativo ou zero

---
+++ exercicio-8.js
@@ -0,0 +1,12 @@
+let valor = 10;
+let situacao;
+
+if (valor > 0) {
+    situacao = "positive"
+} else if (valor < 0) {
+    situacao = "negative"
+} else {
+    situacao = "zero"
+}
+
+console.log (situacao)
