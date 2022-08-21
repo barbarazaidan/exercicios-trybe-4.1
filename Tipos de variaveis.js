dd5de159ef70a356e3a930b5e8d07539705e8c51
bárbara-valdez-zaidan <babivaldez@gmail.com>
2022-08-21 10:00:28 -0300

    criei um arquivo para identificar tipos de variáveis

---
+++ Tipos de variaveis.js
@@ -0,0 +1,14 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+console.log ("tipo de pacientID: ", typeof patientId);
+console.log ("tipo de isEnrolled: ",typeof isEnrolled);
+console.log ("tipo de patientInfo: ",typeof patientInfo);
+console.log ("tipo de patientEmail: ",typeof patientEmail);
+console.log ("tipo de patientAge: ",typeof patientAge);
+patientId = '50';
+console.log ("tipo de patientId: ",typeof patientId);
