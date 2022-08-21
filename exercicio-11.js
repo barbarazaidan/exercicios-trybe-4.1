8da8e5b2f0539c59b5dc2b1e784567e2ef178a76
bárbara-valdez-zaidan <babivaldez@gmail.com>
2022-08-21 18:34:09 -0300

    criei um programa para converter uma nota dada em porcentagem em conceitos A a F

---
+++ exercicio-11.js
@@ -0,0 +1,19 @@
+let porcentagem = prompt ("Digite a nota:");
+let conceito = "";
+
+if (porcentagem < 0 || porcentagem > 100) {
+    console.log ("A nota informada é inválida!")
+} else if (porcentagem >= 90) {
+    conceito ="A"; 
+} else if (porcentagem >= 80) {
+    conceito ="B"; 
+} else if (porcentagem >= 70) {
+    conceito ="C";  
+} else if (porcentagem >= 60) {
+    conceito ="D"; 
+} else if (porcentagem >= 50) {
+    conceito ="E"; 
+} else if (porcentagem < 50) {
+    conceito ="F"; 
+} 
+console.log (conceito)
