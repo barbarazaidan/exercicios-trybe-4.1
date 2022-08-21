75e0d802e1a5f1af338ecc3c507effd53419d61c
bárbara-valdez-zaidan <babivaldez@gmail.com>
2022-08-21 10:06:10 -0300

    criação de arquivo para trabalhar com operadores aritméticos

---
+++ operadores-aritmeticos.js
@@ -0,0 +1,7 @@
+const base = 5;
+let height = 8;
+console.log (base, height);
+const area = base * height;
+console.log (area);
+const perimeter = (2 * base) + (2 * height);
+console.log (perimeter)
