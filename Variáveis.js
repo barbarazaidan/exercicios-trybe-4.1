45d151116c69cbdcdcee4bb4af48eb0e2d4d7e34
bárbara-valdez-zaidan <babivaldez@gmail.com>
2022-08-21 09:49:08 -0300

    fiz os testes alterando valores de variáveis e constantes

--- Variáveis.js
+++ Variáveis.js
@@ -2,3 +2,6 @@ const myName = "Bárbara Valdez Zaidan";
 const birthCity = "Recife";
 let birthYear = 1990;
 console.log (myName, birthCity, birthYear);
+birthYear = 2030;
+console.log (birthYear);
+birthCity = "São Paulo";
